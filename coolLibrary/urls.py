e1fbdddf3d2f3a3780c51f623a68f7bb5e758d47
CeciliaManzoni <xpeppers@xpepperss-MacBook-Pro.local>
2019-12-17 15:49:09 +0100

    change polls dir name

---
+++ coolLibrary/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'coolLibrary'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name= 'index'),
+    path('<int:pk>/', views.SelectionView.as_view(), name='selection'),
+    path('authentication', views.AuthenticationView.as_view(), name='authentication'),
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('donateBook/', views.DonateBookView.as_view(), name='donateBook'),
+    path('<int:book_id>/checkLogin/', views.checkLogin, name='checkLogin'),
+    path('loginUser/', views.loginUser, name='loginUser'),
+    path('logoutUser/', views.logoutUser, name='logoutUser'),
+    path('addNewUser/', views.addNewUser, name='addNewUser'),
+    path('<int:book_id>/manageLoans/', views.manageLoans, name='manageLoans'),
+    path('insertBookInLibrary/', views.insertBookInLibrary, name='insertBookInLibrary'),
+]
