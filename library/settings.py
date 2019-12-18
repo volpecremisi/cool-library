b67e7c3187213516df14183d95eeda613aea1cdf
CeciliaManzoni <xpeppers@xpepperss-MacBook-Pro.local>
2019-12-18 17:02:33 +0100

    try to fix whitenoise

--- library/settings.py
+++ library/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'coolLibrary.apps.LibraryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
